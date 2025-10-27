69c95e78467050aaa09c87408f5adc1188208e75
BlueNyang <xpsj20@gmail.com>
2025-10-28 03:08:06 +0900

    chore: configure Spring Boot server project
    - dependencies
      - Spring Web
      - Spring Security
      - OAuth2 Client
      - Spring Data JPA
      - Oracle Driver
      - Validation
      - Spring AI - Vertex Ai Gemini
      - Spring Boot DevTools
      - Lombok
      - Configuration Processor
      - Data REST
      - JWT(jjwt)
      - SpringDoc (OpenAPI/SwaggerUI)
      - Flyway
      - MapStruct

---
+++ src/main/java/com/multicampus/lc/gamesungcoding/a11ymarketserver/A11ymarketServerApplication.java
@@ -0,0 +1,13 @@
+package com.multicampus.lc.gamesungcoding.a11ymarketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A11ymarketServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A11ymarketServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/multicampus/lc/gamesungcoding/a11ymarketserver/A11ymarketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multicampus.lc.gamesungcoding.a11ymarketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A11ymarketServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
