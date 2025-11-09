54916b38b86bdfc819a7dd8a35e2d5bed14b3146
BlueNyang <xpsj20@gmail.com>
2025-11-09 16:17:16 +0900

    feat: add change permission feature
    - add url mappings for admin features
    - add change user permission feature
    - not tested yet
    
    Ref #51 #52 #53

--- src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/admin/user/service/AdminUserManageService.java
+++
@@ -1,9 +0,0 @@
-package com.multicampus.gamesungcoding.a11ymarketserver.admin.user.service;
-
-import com.multicampus.gamesungcoding.a11ymarketserver.user.model.User;
-
-import java.util.List;
-
-public interface AdminUserManageService {
-    List<User> listAll();
-}
