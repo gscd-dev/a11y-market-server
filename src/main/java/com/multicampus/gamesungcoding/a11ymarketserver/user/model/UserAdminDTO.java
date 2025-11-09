30b60b455febe19979ebb49d875ba3eb66ddfe40
BlueNyang <xpsj20@gmail.com>
2025-11-10 00:16:21 +0900

    refactor: adjust type and add comment
    - refactor type for id into UUID

--- src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/user/model/UserAdminDTO.java
+++ src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/user/model/UserAdminDTO.java
@@ -4,22 +4,25 @@ import lombok.Builder;
 import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @Builder
 public class UserAdminDTO {
-    private String userId;
+    private UUID userId;
     private String userName;
     private String userEmail;
     private String userNickname;
     private String userRole;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    private Date createdAt;
+    private LocalDateTime createdAt;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    private Date updatedAt;
+    private LocalDateTime updatedAt;
 
-    public static UserAdminDTO fromEntity(User user) {
+    // 향후 User 클래스로 위임 예정
+    // 테스트를 위해 DTO에 추가
+    public static UserAdminDTO fromEntity(Users user) {
         // 이름을 홍*동 형태로 마스킹
         String maskedName = user.getUserName();
         for (int i = 1; i < maskedName.length() - 1; i++) {
