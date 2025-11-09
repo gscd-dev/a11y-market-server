b4c3b243c2400eedf1e1aeda240e9168ebde0e0c
BlueNyang <xpsj20@gmail.com>
2025-11-09 17:30:10 +0900

    refactor: remove unnecessary interface for AdminUserManageService
    - remove interface

--- src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/admin/service/AdminUserManageService.java
+++ src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/admin/service/AdminUserManageService.java
@@ -1,11 +1,29 @@
 package com.multicampus.gamesungcoding.a11ymarketserver.admin.service;
 
 import com.multicampus.gamesungcoding.a11ymarketserver.user.model.UserAdminDTO;
+import com.multicampus.gamesungcoding.a11ymarketserver.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface AdminUserManageService {
-    List<UserAdminDTO> listAll();
+@Service
+@RequiredArgsConstructor
+public class AdminUserManageService {
+    private final UserRepository userRepository;
 
-    String changePermission(String userId, String role);
+    public List<UserAdminDTO> listAll() {
+        return userRepository.findAll()
+                .stream()
+                .map(UserAdminDTO::fromEntity)
+                .toList();
+    }
+
+    public String changePermission(String userId, String role) {
+        userRepository.findById(userId).ifPresent(user -> {
+            user.changeRole(role);
+            userRepository.save(user);
+        });
+        return "SUCCESS";
+    }
 }
