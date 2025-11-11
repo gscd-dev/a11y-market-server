0a633e46df70982607cc03058461c1fc87af2155
BlueNyang <xpsj20@gmail.com>
2025-11-11 11:44:57 +0900

    fix: alter zipcode data-type to char(5) from int

--- sql/2.1.addresses.sql
+++ sql/2.1.addresses.sql
@@ -4,10 +4,17 @@ CREATE TABLE addresses (
    address_name     VARCHAR2(100) NOT NULL,
    receiver_name    VARCHAR2(30) NOT NULL,
    receiver_phone   VARCHAR2(15) NOT NULL,
-   receiver_zipcode INT NOT NULL,
+   receiver_zipcode CHAR(5) NOT NULL,
    receiver_addr1   VARCHAR2(100) NOT NULL,
    receiver_addr2   VARCHAR2(200),
    created_at       TIMESTAMP NOT NULL,
    CONSTRAINT fk_address_user FOREIGN KEY ( user_id )
       REFERENCES users ( user_id )
 );
+
+-- alter int to char(5) for receiver_zipcode
+-- temp column -> copy data -> drop old column -> rename temp column
+-- ALTER TABLE addresses ADD (receiver_zipcode_tmp CHAR(5));
+-- UPDATE addresses SET receiver_zipcode_tmp = lpad(to_char(receiver_zipcode), 5, '0');
+-- ALTER TABLE addresses DROP COLUMN receiver_zipcode;
+-- ALTER TABLE addresses RENAME COLUMN receiver_zipcode_tmp TO receiver_zipcode;
