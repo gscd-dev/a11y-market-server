f51804c1a3247bb4da1399fae2fb52a9e28fadf5
BlueNyang <xpsj20@gmail.com>
2025-11-18 01:36:49 +0900

    feat: jwt properties

--- src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/A11ymarketServerApplication.java
+++ src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/A11ymarketServerApplication.java
@@ -1,12 +1,13 @@
 package com.multicampus.gamesungcoding.a11ymarketserver;
 
-import com.multicampus.gamesungcoding.a11ymarketserver.common.config.properties.CorsProperties;
+import com.multicampus.gamesungcoding.a11ymarketserver.common.properties.CorsProperties;
+import com.multicampus.gamesungcoding.a11ymarketserver.common.properties.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(CorsProperties.class)
+@EnableConfigurationProperties({CorsProperties.class, JwtProperties.class})
 public class A11ymarketServerApplication {
 
     public static void main(String[] args) {
