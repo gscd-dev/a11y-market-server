cfa1211c16aa050cd4bd41d5fa61b4f45dc03549
Shin Un Yong <dnsdyd4365@naver.com>
2025-11-21 19:02:19 +0900

    feat: Add order list feature
    - OrderItemsRepository 신규 생성
    - SellerOrderItemResponse DTO 추가
    - SellerService에 받은 주문 목록 조회 로직 구현
    - SellerController에 GET /api/v1/seller/orders 엔드포인트 추가
    
    Resolves #50

--- src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/feature/order/repository/OrderItemsRepository.java
+++ src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/feature/order/repository/OrderItemsRepository.java
@@ -1,9 +1,15 @@
 package com.multicampus.gamesungcoding.a11ymarketserver.feature.order.repository;
 
+import com.multicampus.gamesungcoding.a11ymarketserver.feature.order.entity.OrderItemStatus;
 import com.multicampus.gamesungcoding.a11ymarketserver.feature.order.entity.OrderItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface OrderItemsRepository extends JpaRepository<OrderItems, UUID> {
+
+    List<OrderItems> findByProductIdIn(List<UUID> productIds);
+
+    List<OrderItems> findByProductIdInAndOrderItemStatus(List<UUID> productIds, OrderItemStatus status);
 }
