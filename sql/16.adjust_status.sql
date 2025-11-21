a6be4e0327051258127df43d97a5a57c5e01437f
BlueNyang <xpsj20@gmail.com>
2025-11-22 04:35:15 +0900

    feat: add sql for adjust dummy data as enums and entities

---
+++ sql/16.adjust_status.sql
@@ -0,0 +1,32 @@
+UPDATE addresses
+SET receiver_phone = REPLACE(receiver_phone, '-', '')
+WHERE LENGTH(receiver_phone) = 13;
+COMMIT;
+
+UPDATE orders
+SET receiver_phone = REPLACE(receiver_phone, '-', '')
+WHERE LENGTH(receiver_phone) = 13;
+
+UPDATE orders
+SET receiver_phone = CASE
+                         WHEN LENGTH(receiver_phone) = 13 THEN REPLACE(receiver_phone, '-', '')
+                         ELSE receiver_phone
+    END,
+    order_status   = CASE
+                         WHEN order_status = 'PAID_PENDING' THEN 'PENDING'
+                         ELSE order_status
+        END
+WHERE (LENGTH(전화번호컬럼) = 13)
+   OR (order_status = 'PAID_PENDING');
+COMMIT;
+
+UPDATE sellers
+SET business_number = REPLACE(business_number, '-', ''),
+    seller_grade    = CASE
+                          WHEN seller_grade = 'BRONZE' THEN 'CERTIFIED'
+                          WHEN seller_grade = 'SILVER' THEN 'SUPERIORITY'
+                          ELSE seller_grade
+        END
+WHERE (business_number LIKE '%-%')
+   OR (seller_grade IN ('BRONZE', 'SILVER'));
+COMMIT;
