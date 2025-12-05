063a8ded3e725d1968ed62afc6c875669f53367d
BlueNyang <dksrbxo@bluenyang.kr>
2025-12-05 13:12:56 +0900

    rename: separate package

--- src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/feature/main/entity/MonthlyPopularProduct.java
+++ src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/feature/main/entity/MonthlyPopularProduct.java
@@ -1,4 +1,4 @@
-package com.multicampus.gamesungcoding.a11ymarketserver.feature.main.model;
+package com.multicampus.gamesungcoding.a11ymarketserver.feature.main.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/feature/main/repository/MonthlyPopularProductRepository.java
+++ src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/feature/main/repository/MonthlyPopularProductRepository.java
@@ -1,6 +1,6 @@
 package com.multicampus.gamesungcoding.a11ymarketserver.feature.main.repository;
 
-import com.multicampus.gamesungcoding.a11ymarketserver.feature.main.model.MonthlyPopularProduct;
+import com.multicampus.gamesungcoding.a11ymarketserver.feature.main.entity.MonthlyPopularProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
