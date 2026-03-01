99a4b37eacf13d1ca626979f7a1267841c73557e
BlueNyang <xpsj20@gmail.com>
2026-03-01 18:43:31 +0900

    migrate: convert oauth login handler

--- src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/feature/user/repository/UserOauthLinksRepository.kt
+++ src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/feature/user/repository/UserOauthLinksRepository.kt
@@ -1,19 +1,12 @@
-package com.multicampus.gamesungcoding.a11ymarketserver.feature.user.repository;
+package com.multicampus.gamesungcoding.a11ymarketserver.feature.user.repository
 
-import com.multicampus.gamesungcoding.a11ymarketserver.feature.user.entity.UserOauthLinks;
-import com.multicampus.gamesungcoding.a11ymarketserver.feature.user.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import com.multicampus.gamesungcoding.a11ymarketserver.feature.user.entity.UserOauthLinks
+import com.multicampus.gamesungcoding.a11ymarketserver.feature.user.entity.Users
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
-import java.util.Optional;
-import java.util.UUID;
+interface UserOauthLinksRepository : JpaRepository<UserOauthLinks, UUID> {
+    fun findByOauthProviderId(providerId: String?): UserOauthLinks?
 
-public interface UserOauthLinksRepository extends JpaRepository<UserOauthLinks, UUID> {
-
-    @Query("SELECT l FROM UserOauthLinks l JOIN FETCH l.user WHERE l.oauthProviderId = :providerId")
-    Optional<UserOauthLinks> findByOauthProviderId(String providerId);
-
-    Optional<UserOauthLinks> findByUser(Users users);
-
-    boolean existsByUser(Users user);
+    fun existsByUser(user: Users): Boolean
 }
